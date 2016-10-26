51016d12a48ce05760dc47ad1e40cc9b23741bca
wxiao <wangxiao117210@zongheng.com>
2016-10-26 19:19:05 +0800

    添加测试控制器

---
+++ MBProgress-Demo/Demo/JRTestDemoViewController.h
@@ -0,0 +1,13 @@
+//
+//  JRTestDemoViewController.h
+//  MBProgress-Demo
+//
+//  Created by 王潇 on 16/10/26.
+//  Copyright © 2016年 王潇. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JRTestDemoViewController : UIViewController
+
+@end
---
+++ MBProgress-Demo/Demo/JRTestDemoViewController.m
@@ -0,0 +1,37 @@
+//
+//  JRTestDemoViewController.m
+//  MBProgress-Demo
+//
+//  Created by 王潇 on 16/10/26.
+//  Copyright © 2016年 王潇. All rights reserved.
+//
+
+#import "JRTestDemoViewController.h"
+
+@interface JRTestDemoViewController ()
+
+@end
+
+@implementation JRTestDemoViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
